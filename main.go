e3d10613ce98518c29c6925c5c25e0b6c80f1e4c
Nick <nick1729_github@mail.ru>
2021-04-24 22:47:31 +0300

    3

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
@@ -48,9 +52,44 @@ func main() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = fmt.Sprint("Available commands:",
+					"\n/weather - show current weather",
+					"\n/city - set name of the city",
+					"\n/coordinates - set coordinates",
+					"\n/units - set measurement units",
+					"\n/lang - set language")
+			case "weather":
+				msg.Text = "Loading weather data..."
+			case "city":
+				c.City = update.Message.CommandArguments()
+			case "coordinates":
+				s := update.Message.CommandArguments()
+				args := strings.Split(s, ", ")
+				if len(args) == 2 {
+					c.Coord.Lon, _ = strconv.ParseFloat(args[0], 64)
+					c.Coord.Lat, _ = strconv.ParseFloat(args[1], 64)
+				}
+				msg.Text = fmt.Sprintf("Lon %f, Lat %f", c.Coord.Lon, c.Coord.Lat)
+			case "units":
+				c.Units = update.Message.CommandArguments()
+				msg.Text = fmt.Sprintf("Units = %s", c.Units)
+			case "lang":
+				c.Lang = update.Message.CommandArguments()
+				msg.Text = fmt.Sprintf("Lang = %s", c.Lang)
+			case "exit":
+				log.Println("Exit")
+				os.Exit(1)
+			default:
+				msg.Text = "Wrong command. Type /help"
+			}
+		} else {
+			msg.Text = "Type /help"
+		}
 		bot.Send(msg)
 	}
 }
--- types.go
+++ types.go
@@ -1,5 +1,65 @@
 package main
 
 type tConfig struct {
-	Token string `json:"tg_token"`
+	Token  string `json:"tg_token"`
+	ApiURL string `json:"weather_api_url"`
+	ApiKEY string `json:"api_key"`
+	City   string `json:"location"`
+
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coordinates"`
+
+	Units string `json:"measurement_units"`
+	Lang  string `json:"language"`
+}
+
+type tWeather struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+
+	Visibility int `json:"visibility"`
+
+	Wind struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+
+	Dt int `json:"dt"`
+
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
 }
